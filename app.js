6270e102929dfb0d93e7c73432a9a13a140257db
bote795 <bote795@tamu.edu>
2015-12-22 15:50:15 -0600

    fixed package desc and added env var for db

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
 
 //mongoose
-mongoose.connect('mongodb://localhost/passport_local_mongoose_express42');
+mongoose.connect(process.env.DB ||'mongodb://localhost/passport_local_mongoose_express42');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
